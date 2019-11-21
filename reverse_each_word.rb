5944fc77d14f1e148cbba71d274594c7482e5978
Sana Zaidi <sana.rizvi.5@gmail.com>
2019-11-21 01:40:37 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
-sentence1 = "Flip this sentence!"
+sentence1 = "Hello there, and how are you?"
 def reverse_each_word(sentence1) 
   sentence1.collect do |sentence1| 
-    "Flip this sentence!"
+    "Hello there, and how are you?"
 end 
+
 end 
