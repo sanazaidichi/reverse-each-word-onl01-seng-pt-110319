da9b1969d4798b5749923bb7adca24ca0ddb4996
Sana Zaidi <sana.rizvi.5@gmail.com>
2019-11-21 01:42:05 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 sentence1 = "Hello there, and how are you?"
 def reverse_each_word(sentence1) 
-  sentence1.collect do |sentence1| 
+  sentence1.each do |sentence1| 
     "Hello there, and how are you?"
 end 
 
